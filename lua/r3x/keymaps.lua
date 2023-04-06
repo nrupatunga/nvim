644151ed009f32f444a7309cf06c2ff00e4ba581
nrupatunga <nrupatunga.tunga@gmail.com>
2023-04-06 22:17:14 +0530

    update toggleterm

--- lua/r3x/keymaps.lua
+++ lua/r3x/keymaps.lua
@@ -81,10 +81,3 @@ function _G.toggle_diagnostics()
 end
 
 keymap("n", "<leader>D", ":call v:lua.toggle_diagnostics()<CR>", { silent = true, noremap = true })
-
--- toggleterm
--- lazygit
-keymap("n", "<leader>lg", ":call v:lua._LAZYGIT_TOGGLE()<CR>", { silent = true, noremap = true })
---
-keymap("n", "<leader>lt", ":call v:lua._HTOP_TOGGLE()<CR>", { silent = true, noremap = true })
-keymap("n", "<leader>ll", ":call v:lua._PYTHON_TOGGLE()<CR>", { silent = true, noremap = true })
--- lua/r3x/plugins/toggleterm.lua
+++ lua/r3x/plugins/toggleterm.lua
@@ -1,51 +1,60 @@
+function _G.set_terminal_keymaps()
+    local opts = { noremap = true }
+    vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
+end
+
 return {
     "akinsho/toggleterm.nvim",
     event = "BufReadPre",
     opts = {
         size = 15,
-        open_mapping = [[<M-t>]],
+        open_mapping = [[<c-\>]],
+        hide_numbers = true,
         shade_filetypes = {},
         shade_terminals = true,
-        shading_factor = "1",
-        close_on_exit = false,
+        shading_factor = 2,
         start_in_insert = true,
+        insert_mappings = true,
         persist_size = true,
         direction = "float",
+        close_on_exit = true,
+        shell = vim.o.shell,
         float_opts = {
             border = "curved",
             winblend = 0,
+            highlights = {
+                border = "Normal",
+                background = "Normal",
+            },
         },
     },
-    config = function()
-        function _G.set_terminal_keymaps()
-            local opts = { noremap = true }
-            --vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
-            --vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]], opts)
-            vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
-            vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
-            vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
-            vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
-        end
+    config = function(_, opts)
+        require("toggleterm").setup(opts)
+        vim.cmd("autocmd! TermOpen term://* lua _G.set_terminal_keymaps()")
 
-        vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
-
+        -- Set up lazygit inside a toggleterm instance
         local Terminal = require("toggleterm.terminal").Terminal
-        local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
 
         function _LAZYGIT_TOGGLE()
+            local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, dir = vim.fn.getcwd() })
             lazygit:toggle()
         end
 
-        local htop = Terminal:new({ cmd = "htop", hidden = true })
+        vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", { noremap = true, silent = true })
 
         function _HTOP_TOGGLE()
+            local htop = Terminal:new({ cmd = "htop", hidden = true, dir = vim.fn.getcwd() })
             htop:toggle()
         end
+        vim.api.nvim_set_keymap("n", "<leader>ht", "<cmd>lua _HTOP_TOGGLE()<CR>", { noremap = true, silent = true })
 
-        local python = Terminal:new({ cmd = "ipython", hidden = true })
-
-        function _PYTHON_TOGGLE()
+        function _IPYTHON_TOGGLE()
+            local python = Terminal:new({ cmd = "ipython", hidden = true, dir = vim.fn.getcwd() })
             python:toggle()
         end
+        vim.api.nvim_set_keymap("n", "<leader>ll", "<cmd>lua _IPYTHON_TOGGLE()<CR>", { noremap = true, silent = true })
     end,
 }
