44e8955937fae3fbe93293dca8e0bb7ea6cbb702
nrupatunga <nrupatunga.tunga@gmail.com>
2023-04-06 20:45:56 +0530

    disable diagnostics for buffer

--- lua/r3x/handlers.lua
+++ lua/r3x/handlers.lua
@@ -40,13 +40,14 @@ M.setup = function()
     })
 
     vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-        signs = true,
-        underline = true,
-        virtual_text = {
-            spacing = 5,
-            severity_limit = "Hint",
-        },
-        update_in_insert = true,
+        virtual_text = false,
+        --signs = true,
+        --underline = true,
+        --virtual_text = {
+        --spacing = 5,
+        --severity_limit = "Hint",
+        --},
+        --update_in_insert = true,
     })
 
     vim.api.nvim_create_autocmd("TextYankPost", {
