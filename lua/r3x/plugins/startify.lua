5d44deda8385b3c80e6950de3568de4d0265dda2
nrupatunga <nrupatunga.tunga@gmail.com>
2023-04-17 11:48:59 +0530

    update startify for minimal rendering

--- lua/r3x/plugins/startify.lua
+++ lua/r3x/plugins/startify.lua
@@ -1,3 +1,13 @@
 return {
-    "mhinz/vim-startify",
+    "goolord/alpha-nvim",
+    requires = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        local alpha = require("alpha")
+        local startify = require("alpha.themes.startify")
+
+        startify.section.header.val = {}
+	startify.section.mru.val = { { type = "padding", val = 0 } }
+        startify.nvim_web_devicons.enabled = false
+        alpha.setup(startify.config)
+    end,
 }
