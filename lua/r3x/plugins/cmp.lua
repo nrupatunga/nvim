bdc5dcf4e5f09ed5c1e227fabdcbfe2a9d8b1b8e
nrupatunga <nrupatunga.tunga@gmail.com>
2023-10-06 12:08:02 +0530

    update cmp.lua

--- lua/r3x/plugins/cmp.lua
+++ lua/r3x/plugins/cmp.lua
@@ -1,12 +1,13 @@
 return {
     "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
+    event = { 'InsertEnter', 'BufReadPre', 'BufNewFile'},
+    --event = { "InsertEnter" },
     dependencies = {
+        "L3MON4D3/LuaSnip",
         "hrsh7th/cmp-nvim-lsp",
+        "saadparwaiz1/cmp_luasnip",
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-buffer",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
         "hrsh7th/cmp-nvim-lsp-signature-help",
     },
     config = function()
