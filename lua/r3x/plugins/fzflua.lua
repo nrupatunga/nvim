2ad683ded9b94709c3268e4d02373b66db728271
nrupatunga <nrupatunga.tunga@gmail.com>
2023-12-22 10:55:54 +0530

    fzf keymaps

--- lua/r3x/plugins/fzflua.lua
+++ lua/r3x/plugins/fzflua.lua
@@ -1,9 +1,16 @@
 return {
-  "ibhagwan/fzf-lua",
-  -- optional for icon support
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    -- calling `setup` is optional for customization
-    require("fzf-lua").setup({})
-  end
+    "ibhagwan/fzf-lua",
+    -- optional for icon support
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        -- calling `setup` is optional for customization
+        require("fzf-lua").setup({})
+    end,
+    keys = {
+        {
+            "<leader>S",
+            "<cmd>lua require('fzf-lua').grep_cword()<cr>",
+            desc = "Find word under cursor",
+        },
+    },
 }
