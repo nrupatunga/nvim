7cb332cda9247919f5d8d1c84b79adf99ce83b10
nrupatunga <nrupatunga.tunga@gmail.com>
2024-01-05 18:47:06 +0530

    add git as root for harpoon

--- lua/r3x/plugins/harpoon.lua
+++ lua/r3x/plugins/harpoon.lua
@@ -6,7 +6,18 @@ return {
         "nvim-lua/plenary.nvim",
     },
     config = function()
-        require("harpoon"):setup()
+        require("harpoon"):setup({
+            default = {
+                get_root_dir = function()
+                    local cwd = vim.loop.cwd()
+                    local root = vim.fn.system("git rev-parse --show-toplevel")
+                    if vim.v.shell_error == 0 and root ~= nil then
+                        return string.gsub(root, "\n", "")
+                    end
+                    return cwd
+                end,
+            },
+        })
         require("telescope").load_extension("harpoon")
     end,
     keys = {
