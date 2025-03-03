a039b25abe560fe4ea610032008787208db73318
Nrupatunga <nrupatunga.tunga@gmail.com>
2025-03-03 10:10:25 +0530

    add cuda to treesitter

--- lua/r3x/plugins/treesitter.lua
+++ lua/r3x/plugins/treesitter.lua
@@ -16,6 +16,7 @@ return {
                 ensure_installed = {
                     "bash",
                     "c",
+                    "cuda",
                     "cpp",
                     "css",
                     "dart",
