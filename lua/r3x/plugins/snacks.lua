cc74c562c5a887824fe7ec16a1400bfb901d633f
nrupatunga <nrupatunga.tunga@gmail.com>
2025-03-28 19:23:32 +0530

    make snacks not lazy

--- lua/r3x/plugins/snacks.lua
+++ lua/r3x/plugins/snacks.lua
@@ -1,7 +1,7 @@
 return {
     "folke/snacks.nvim",
     priority = 1000,
-    lazy = true,
+    lazy = false,
     opts = {
         -- your configuration comes here
         -- or leave it empty to use the default settings
