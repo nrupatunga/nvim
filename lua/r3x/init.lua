9fc9611e3ecdb94d9e2db34d342211256aa60465
nrupatunga <nrupatunga.tunga@gmail.com>
2025-05-23 13:17:13 +0530

    update

--- lua/r3x/init.lua
+++ lua/r3x/init.lua
@@ -3,10 +3,12 @@ require("r3x.keymaps")
 require("r3x.lazy")
 
 --vim.cmd([[colorscheme darkplus]])
-vim.cmd([[colorscheme github_dark_high_contrast]])
+--vim.cmd([[colorscheme vague]])
+--vim.cmd([[colorscheme github_dark_high_contrast]])
+vim.cmd([[colorscheme gruvbox]])
 --vim.cmd([[colorscheme night-owl]])
 --vim.cmd([[colorscheme carbonfox]])
-if vim.g.colors_name == "github_dark_high_contrast" then
+if vim.g.colors_name == "gruvbox" then
     vim.cmd([[highlight Comment cterm=italic gui=italic guifg=#838383]])
     vim.cmd([[highlight LineNr guifg=#838383]])
 end
--- lua/r3x/plugins/themes.lua
+++ lua/r3x/plugins/themes.lua
@@ -3,6 +3,8 @@ return {
     "nrupatunga/night-owl.nvim",
     "projekt0n/github-nvim-theme",
     "nrupatunga/darkplus.nvim",
-		"EdenEast/nightfox.nvim",
-		"wuelnerdotexe/vim-enfocado"
+    "EdenEast/nightfox.nvim",
+    "wuelnerdotexe/vim-enfocado",
+    "vague2k/vague.nvim",
+    "morhetz/gruvbox",
 }
